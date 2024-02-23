1decf919837cce303d311d2f8e368decd5689cb4
Matthew <matthha@umich.edu>
2024-02-23 17:28:29 -0500

    "Finish Tutorial"

--- src/main.jsx
+++ src/main.jsx
@@ -4,26 +4,92 @@ import ReactDOM from 'react-dom/client'
 import {
   createBrowserRouter,
   RouterProvider,
+  createRoutesFromElements,
 } from "react-router-dom";
 import './index.css'
-import Root from './routes/root';
+import Root, { loader as rootLoader, action as rootAction } from './routes/root';
 import ErrorPage from './error-page';
-import Contact from './routes/contact';
+import Contact, { loader as contactLoader, action as contactAction, } from './routes/contact';
+import EditContact, { action as editAction } from './routes/edit';
+import { action as destroyAction } from './routes/destroy';
+import Index from './routes/index';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
     children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          { index: true, element: <Index /> },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+      {
+        index: true, element: <Index />,
+      },
       {
         path: "contacts/:contactId",
         element: <Contact />,
+        loader: contactLoader,
+        action: contactAction,
       },
+      {
+        path: "contacts/:contactId/edit",
+        element: <EditContact />,
+        loader: contactLoader,
+        action: editAction,
+      },
+      {
+        path: 'contacts/:contactId/destroy',
+        action: destroyAction,
+        errorElement: <div>Ooops! There was an error.</div>
+      }
+    ],
+  },
     ]
   },
 
 ]);
+// Above is one way to make the router and below is the other using createRoutesFromElements;
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route
+//       path="/"
+//       element={<Root />}
+//       loader={rootLoader}
+//       action={rootAction}
+//       errorElement={<ErrorPage />}
+//     >
+//       <Route errorElement={<ErrorPage />}>
+//         <Route index element={<Index />} />
+//         <Route
+//           path="contacts/:contactId"
+//           element={<Contact />}
+//           loader={contactLoader}
+//           action={contactAction}
+//         />
+//         <Route
+//           path="contacts/:contactId/edit"
+//           element={<EditContact />}
+//           loader={contactLoader}
+//           action={editAction}
+//         />
+//         <Route
+//           path="contacts/:contactId/destroy"
+//           action={destroyAction}
+//         />
+//       </Route>
+//     </Route>
+//   )
+// );
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,46 +1,108 @@
-import { Outlet, Link } from "react-router-dom";
+import { useEffect } from "react";
+import { Outlet, NavLink, Link, useLoaderData, Form, redirect, useNavigation, useSubmit } from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
 
+export async function action() {
+  const contact = await createContact();
+  return redirect(`/contacts/${contact.id}/edit`);
+}
+export async function loader({ request }) {
+  const url = new URL(request.url);
+  const q = url.searchParams.get("q");
+  const contacts = await getContacts(q);
+  return { contacts, q };
+}
 
 export default function Root() {
+  const { contacts, q } = useLoaderData();
+  const navigation = useNavigation();
+  const submit = useSubmit();
+
+  const searching = 
+    navigation.location &&
+    new URLSearchParams(navigation.location.search).has('q');
+
+  useEffect(() => {
+    document.getElementById('q').value = q;
+  }, [q]);
    return (
      <>
        <div id="sidebar">
          <h1>React Router Contacts</h1>
          <div>
-           <form id="search-form" role="search">
+           <Form id="search-form" role="search">
              <input
+            //  can change q's to be lookup or search
                id="q"
+               className={searching?'loading':''}
                aria-label="Search contacts"
                placeholder="Search"
                type="search"
                name="q"
+               defaultValue={q}
+              //  This will also make the URL update after every button meaning the back button has to be pressed a lot for long queries. This is why we add the replace to it.
+               onChange={(event) => {
+                const isFirstSearch = q == null;
+                submit(event.currentTarget.form, {
+                  replace: !isFirstSearch,
+                });
+               }}
              />
              <div
                id="search-spinner"
                aria-hidden
-               hidden={true}
+               hidden={!searching}
              />
              <div
                className="sr-only"
                aria-live="polite"
              ></div>
-           </form>
-           <form method="post">
+           </Form>
+           <Form method="post">
              <button type="submit">New</button>
-           </form>
+           </Form>
          </div>
          <nav>
-           <ul>
-             <li>
-               <Link to={`/contacts/1`}>Your Name</Link>
-             </li>
-             <li>
-               <Link to={`/contacts/2`}>Your Friend</Link>
-             </li>
-           </ul>
+         {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <NavLink
+                    to={`contacts/${contact.id}`}
+                    className={({ isActive, isPending }) =>
+                      isActive
+                        ? "active"
+                        : isPending
+                        ? "pending"
+                        : ""
+                    }
+                  >
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>                  
+                  </NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
          </nav>
        </div>
-       <div id="detail">
+       <div 
+        id="detail"
+        className={navigation.state === 'loading' ? 'loading' : ''
+          }
+       >
          <Outlet />
        </div>
      </>
